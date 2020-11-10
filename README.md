50f6ccbc9cdfc51f9d2138a856584eb0c7ad7a60
Tommy Willen <tommyallen1215@gmail.com>
2020-11-10 13:54:02 -0800

    fixed validates to include error messages and added text to readme

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# Employee-Summary-Template-Engine
+# Employee Summary Template Engine
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](https://www.contributor-covenant.org/version/2/0/code_of_conduct/code_of_conduct.md)
+
+## Description
+
+This is a command line application designed to create an employee websitethat displays the employee name, role, email, and room number/github username/school (depending on employee type) for each employee in a manager's team.
+
+## Table of Contents
+    
+- [Description](#description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [License](#License)
+- [Contributing](#Contributing)
+- [Tests](#Tests)
+- [Questions](#Questions)
+    
+## Installation
+    
+npm i
+    
+## Usage
+    
+The application utilizes the npm inquirer package to ask the user a given set of prompts about themselves as well as their employees. There are also validation statements that provide error messages if the responses are not the valid type or (in the case of employee id) is already taken. Once the user has entered the information for the manager (user), they will be asked if they would like to add another employee (either an engineer, an intern, or finished). This will ask a new set of questions for about the next employee. After each employee's information is added, all of their details will be added to an array of employees. This array will be called upon after the user has finished adding employees to create a html document. The html document is created by combining the various html templates together to create one large html document with all of the employees information displayed.
+    
+## License
+    
+The project is covered under the MIT license.
+    
+## Contributing
+    
+    
+ 
+This is an open source application and welcome for contribution. If you would like to contribute, you can fork my repo and submit any pull request for any features you would like added.    
+Contributions are protected by the contributor covenant V2.0. If you have any new features you would like to see added or want to report abuse please contact me at tommywillen12@gmail.com 
+Click [here](https://www.contributor-covenant.org/version/2/0/code_of_conduct/code_of_conduct.md) for more information regarding contributor covenant V2.0.
+ 
+    
+## Tests
+    
+npm run test
+    
+## Questions
+    
+GitHub Profile: [TommyWillen](https://github.com/TommyWillen)
+
+Email: tommywillen12@gmail.com
--- app.js
+++ app.js
@@ -11,12 +11,166 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+let employeeList = [];
+// manager prompts
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+let idCheck = val => employeeList.some(employee => (employee.id === val))
+
+const managerFunc = () => {
+    inquirer.prompt([
+        {
+
+            type: "input",
+            name: "name",
+            message: "What is your name?",
+            validate: val => (val.length < 3) ? "Name must be at least 2 or more characters" : true,
+        },
+        {
+
+            type: "input",
+            name: "id",
+            message: "What is your employee id?",
+            validate: val => (isNaN(parseInt(val))) ? "Must be a number":true,
+        },
+        {
+            //   email, officenumber
+            type: "input",
+            name: "email",
+            message: "What is your employee email?",
+            validate: val => (val.length < 3) ? "Name must be at least 2 or more characters" : true,
+        },
+        {
+
+            type: "input",
+            name: "officeNumber",
+            message: "What is your office number?",
+            validate: val => (isNaN(parseInt(val))) ? "Must be a number":true,
+        },
+        {
+            type: "list",
+            name: "employeeAdd",
+            message: "Would you like to add another employee?",
+            choices: [{ name: "Add an engineer to the list", value: "Engineer" }, { name: "Add an intern to the list", value: "Intern" }, { name: "Finished adding employees", value: "None" }]
+        },
+    ]).then(response => {
+        // create manager obj
+        let managerObj = new Manager(response.name, response.id, response.email, response.officeNumber);
+
+        employeeList.push(managerObj);
+
+        switch (response.employeeAdd) {
+            case "Engineer": engineerFunc();
+                break;
+            case "Intern": internFunc();
+                break;
+            case "None":
+                fs.writeFile(outputPath, render(employeeList), err => err ? console.log(err) : console.log("Success"));
+        }
+    })
+}
+
+const engineerFunc = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is their name?",
+            validate: val => (val.length < 3) ? "Name must be at least 2 or more characters" : true,
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is their employee id?",
+            validate: val => (!isNaN(parseInt(val)) || !idCheck(val)) ? "Id is either not a number or already in use":true,
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is their employee email?",
+            validate: val => (val.length < 3) ? "Name must be at least 2 or more characters" : true,
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is their Github username?",
+            validate: val => (val.length < 3) ? "Name must be at least 2 or more characters" : true,
+        },
+        {
+            type: "list",
+            name: "employeeAdd",
+            message: "Would you like to add another employee?",
+            choices: [{ name: "Add an engineer to the list", value: "Engineer" }, { name: "Add an intern to the list", value: "Intern" }, { name: "Finished adding employees", value: "None" }]
+        },
+    ]).then(response => {
+        // create engineer obj
+        let engineerObj = new Engineer(response.name, response.id, response.email, response.github);
+
+        employeeList.push(engineerObj);
+
+        switch (response.employeeAdd) {
+            case "Engineer": engineerFunc();
+                break;
+            case "Intern": internFunc();
+                break;
+            case "None":
+                fs.writeFile(outputPath, render(employeeList), err => err ? console.log(err) : console.log("Success"));
+        }
+
+    })
+}
+
+const internFunc = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is their name?",
+            validate: val => (val.length < 3) ? "Name must be at least 2 or more characters" : true,
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is their employee id?",
+            validate: val => (!isNaN(parseInt(val)) || !idCheck(val)) ? "Id is either not a number or already in use":true,
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is their employee email?",
+            validate: val => (val.length < 3) ? "Name must be at least 2 or more characters" : true,
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What school do they attend?",
+            validate: val => (val.length < 3) ? "Name must be at least 2 or more characters" : true,
+        },
+        {
+            type: "list",
+            name: "employeeAdd",
+            message: "Would you like to add another employee?",
+            choices: [{ name: "Add an engineer to the list", value: "Engineer" }, { name: "Add an intern to the list", value: "Intern" }, { name: "Finished adding employees", value: "None" }]
+        },
+    ]).then(response => {
+        // create engineer obj
+        let engineerObj = new Intern(response.name, response.id, response.email, response.school);
+
+        employeeList.push(engineerObj);
+
+        switch (response.employeeAdd) {
+            case "Engineer": engineerFunc();
+                break;
+            case "Intern": internFunc();
+                break;
+            case "None":
+                fs.writeFile(outputPath, render(employeeList), err => err ? console.log(err) : console.log("Success"));
+        }
+
+    })
+}
+
+managerFunc();
+
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
